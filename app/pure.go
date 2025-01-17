ebf3232a8cc51fbeb8eecdae32fbdf6bece7e1d8
差一点满分 <chayidianmanfen@163.com>
2025-01-17 17:35:06 +0800

    feat: 实现找顺子的代码

--- app/pure.go
+++ app/pure.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 )
 
-func GetPure(rawCards []Card) (pure, overCard []Card) {
+func GetPure(rawCards []Card, isAsc bool) (pure [][]Card, overCard []Card) {
 	// 我们接收一组牌，在这组牌当中找到顺子，返回结果和剩余牌
 	// 为什么要复制一份？因为切片是指针类型，如果直接操作会影响外面的数据
 	// 这里的职责是找到顺子，然后返回结果和剩余其他的不是该函数的要点。
@@ -16,31 +16,52 @@ func GetPure(rawCards []Card) (pure, overCard []Card) {
 	var cards []Card
 	_ = copier.Copy(&cards, &rawCards)
 
+	sort.Slice(cards, func(i, j int) bool {
+		if isAsc {
+			return cards[i].Value() < cards[j].Value()
+		} else {
+			return cards[i].Value() > cards[j].Value()
+		}
+	})
+
+	// 计算因子，为了兼容降序
+	factors := 1
+	if !isAsc {
+		factors = -1
+	}
+
 	for {
 		if len(cards) < 3 {
+			overCard = append(overCard, cards...)
 			break
 		}
 
-		minCard := cards[0]
-		maxCard := cards[0]
-
-		for _, c := range cards {
-			if c.Value() < minCard.Value() {
-				minCard = c
-			}
-
-			if c.Value() > maxCard.Value() {
-				maxCard = c
+		// 2. 比较，找到连续的值
+		seq := []Card{cards[0]}
+		for i := 1; i < len(cards); i++ {
+			if cards[i].Value()-seq[len(seq)-1].Value() == 1*factors {
+				seq = append(seq, cards[i])
+			} else {
+				break
 			}
 		}
 
-		isSeq := maxCard.Value()-minCard.Value() == len(cards)-1
-		if !isSeq {
+		if len(seq) >= 3 {
+			if len(seq) >= 6 && (len(seq)%6 == 0 || len(seq)%6 == 2 || len(seq)%6 == 4) {
+				// 如果长度符合条件，将序列分为两部分
+				middle := len(seq) / 2
+				pure = append(pure, seq[:middle], seq[middle:])
+			} else {
+				// 其他情况下，直接加入 pure
+				pure = append(pure, seq)
+			}
+			// 从 cards 中移除 seq
+			cards = difference(cards, seq)
+		} else {
+			// 如果 seq 长度小于 3，直接将第一张卡片加入 overCard
+			// 目的是处理可能顺子的第一张牌并不是顺子的情况
 			overCard = append(overCard, cards[0])
 			cards = cards[1:]
-		} else {
-			pure = append(pure, cards...)
-			cards = difference(cards, pure)
 		}
 	}
 	return pure, overCard
@@ -69,5 +90,13 @@ func difference(a, b []Card) []Card {
 		}
 	}
 
+	return cards
+}
+
+func changeTo1Dim(rawCards [][]Card) []Card {
+	var cards []Card
+	for _, v := range rawCards {
+		cards = append(cards, v...)
+	}
 	return cards
 }
--- main.go
+++ main.go
@@ -10,13 +10,13 @@ func main() {
 	// 当前例子采用方片，1，2，3，5进行示范，找顺子
 
 	// 创建牌对象
-	cards := []app.Card{
-		app.NewCard(1, 0), // 方片 1
-		app.NewCard(2, 0), // 方片 2
-		app.NewCard(3, 0), // 方片 3
-		app.NewCard(4, 0), // 方片 4
-		app.NewCard(5, 0), // 方片 5
-	}
+	//cards := []app.Card{
+	//	app.NewCard(1, 0), // 方片 1
+	//	app.NewCard(2, 0), // 方片 2
+	//	app.NewCard(3, 0), // 方片 3
+	//	app.NewCard(4, 0), // 方片 4
+	//	app.NewCard(5, 0), // 方片 5
+	//}
 
 	cards2 := []app.Card{
 		app.NewCard(1, 0), // 方片 1
@@ -24,11 +24,15 @@ func main() {
 		app.NewCard(3, 0), // 方片 3
 		app.NewCard(4, 0), // 方片 4
 		//app.NewCard(5, 0), // 方片 5
-		app.NewCard(6, 0), // 方片 5
-		app.NewCard(7, 0), // 方片 5
-		app.NewCard(8, 0), // 方片 5
+		app.NewCard(6, 0),  // 方片 5
+		app.NewCard(7, 0),  // 方片 5
+		app.NewCard(8, 0),  // 方片 5
+		app.NewCard(9, 0),  // 方片 5
+		app.NewCard(10, 0), // 方片 5
+		//app.NewCard(11, 0), // 方片 5
+		app.NewCard(12, 0), // 方片 5
 	}
 
-	fmt.Println(app.GetPure(cards))
-	fmt.Println(app.GetPure(cards2))
+	//fmt.Println(app.GetPure(cards))
+	fmt.Println(app.GetPure(cards2, false))
 }
